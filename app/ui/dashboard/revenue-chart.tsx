ac482452965d49e85e1ea1e82722d861cb6c1930
Jeganmahi <116663645+Jeganmahi@users.noreply.github.com>
2024-04-12 15:45:00 +0530

    Update revenue-chart.tsx

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -25,7 +25,7 @@ export default async function RevenueChart({
 
   return (
     <div className="w-full md:col-span-4">
-      <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+      <h2 className={`${secondaryFont.className} mb-4 text-xl md:text-2xl`}>
         Recent Revenue
       </h2>
       {/* NOTE: comment in this code when you get to this point in the course */}
