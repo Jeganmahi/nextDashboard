badf2d40df3b3df0bd8ab8a6868d489dac083e4c
Jeganmahi <116663645+Jeganmahi@users.noreply.github.com>
2024-04-12 15:44:17 +0530

    Update latest-invoices.tsx

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -10,7 +10,7 @@ export default async function LatestInvoices({
 }) {
   return (
     <div className="flex w-full flex-col md:col-span-4">
-      <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+      <h2 className={`${secondaryFont.className} mb-4 text-xl md:text-2xl`}>
         Latest Invoices
       </h2>
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
